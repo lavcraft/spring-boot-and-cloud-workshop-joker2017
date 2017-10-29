c365355058b5efd9437ea022550646523b23502f
Tolkachev Kirill <lavcraft@gmail.com>
2017-10-30 01:20:55 +0300

    #step1 add crud repository and random exercise controller

---
+++ theology-service/src/main/java/ru/spring/ripper/theology/TheologyApplication.java
@@ -0,0 +1,39 @@
+package ru.spring.ripper.theology;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.spring.ripper.examinator.domain.Exercise;
+import ru.spring.ripper.theology.repository.ExerciseRepository;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author tolkv
+ * @version 29/10/2017
+ */
+@SpringBootApplication
+@EntityScan(basePackageClasses = Exercise.class)
+@EnableConfigurationProperties(TheologyApplicationProperties.class)
+public class TheologyApplication {
+  @Autowired
+  ExerciseRepository exerciseRepository;
+  @Autowired
+  TheologyApplicationProperties theologyApplicationProperties;
+
+  @PostConstruct //flyway для бедных
+  public void init() {
+    theologyApplicationProperties.getQuestionsAndAnswers().forEach((q, a) -> {
+      exerciseRepository.save(Exercise.builder()
+          .question(q)
+          .answer(a)
+          .build());
+    });
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheologyApplication.class, args);
+  }
+}
