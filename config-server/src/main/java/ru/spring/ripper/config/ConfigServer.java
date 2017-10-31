0bb1186f7d81b343574045615caf46ebb7ab4ca9
Tolkachev Kirill <lavcraft@gmail.com>
2017-11-01 01:07:15 +0300

    Add eureka support to config-server. Integrate with math service

--- config-server/src/main/java/ru/spring/ripper/config/ConfigServer.java
+++ config-server/src/main/java/ru/spring/ripper/config/ConfigServer.java
@@ -2,6 +2,7 @@ package ru.spring.ripper.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @version 01/11/2017
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServer {
   public static void main(String[] args) {
